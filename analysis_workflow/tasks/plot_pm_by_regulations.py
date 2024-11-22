5c5de02eedc3a323ca9a7c6ca6eb1664983716b1
GerganaTancheva123 <gerganatancheva1@gmail.com>
2024-11-22 15:36:08 +0200

    new plot summarizing all nodes

--- analysis_workflow/tasks/plot_pm_by_regulations.py
+++ analysis_workflow/tasks/plot_pm_by_regulations.py
@@ -1,5 +1,4 @@
 # + tags=["parameters"]
-
 upstream = ['aggregate_nodes']
 product = None
 folder_input = None
@@ -17,13 +16,13 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 
-def pm_by_year_norm(df1, feature1, feature2, date, output_filename):
+def pm_by_year_norm(df, feature1, feature2, date, output_filename):
     """
        The function works as expected when the period is 'Y', 'YE', or 'YS',
        based on yearly regulatory norms for PM 10 and PM 2.5.
        It also works for monthly periods such as 'M', 'ME', and 'MS'
     """
-
+    df1 = df.copy(deep=True)
     fig = go.Figure()
     df1[feature1] = pd.to_numeric(df1[feature1], errors='coerce')
     df1[feature2] = pd.to_numeric(df1[feature2], errors='coerce')
@@ -62,38 +61,85 @@ def pm_by_year_norm(df1, feature1, feature2, date, output_filename):
 
 
 def pm_by_daily_norm(df, feature, data, output_filename):
-
     """
        The function works as expected when the period is 'D',
        based on the daily regulatory norm for PM 10.
     """
+    df1 = df.copy(deep=True)
+    df1.reset_index(inplace=True)
+    df1[data] = pd.to_datetime(df1[data])
+    df1['year'] = df1[data].dt.year
+    df1['month'] = df1[data].dt.month
+    df1['day'] = df1[data].dt.day
+    df1[feature] = pd.to_numeric(df[feature], errors='coerce')
+    df1[feature] = df1[feature].fillna(0)
 
-    df.reset_index(inplace=True)
-    df[data] = pd.to_datetime(df[data])
-    df['year'] = df[data].dt.year
-    df['month'] = df[data].dt.month
-    df['day'] = df[data].dt.day
-    df[feature] = pd.to_numeric(df[feature], errors='coerce')
-    df[feature] = df[feature].fillna(0)
-
-    df['color1'] = df[feature].apply(lambda x: 'Red' if x > 50 else 'Blue')
-    fig = px.scatter(df, x="month", y=feature, facet_col="year", size=feature,
+    df1['color1'] = df1[feature].apply(lambda x: 'Red' if x > 50 else 'Blue')
+    fig = px.scatter(df1, x="month", y=feature, facet_col="year", size=feature,
                      hover_data=['day'], color="color1")
     fig.update_xaxes(matches=None)
     fig.update_layout(showlegend=False)
     fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
 
     fig.write_html(output_filename)
+    return df1
 
 
 csv_files = glob.glob(os.path.join(upstream['aggregate_nodes']['aggregated_nodes'], '*.csv'))
 
 os.makedirs(product['plots'], exist_ok=True)
 
+combined_df = pd.DataFrame()
 for file in csv_files:
     df = pd.read_csv(file)
     file_name = os.path.basename(file)
     output_file = os.path.join(product['plots'], os.path.splitext(file_name)[0])
 
     pm_by_year_norm(df, 'pm_10.0', 'pm_2.5', 'datetime', f'{output_file}_by_year.html')
-    pm_by_daily_norm(df, 'pm_10.0', 'datetime', f'{output_file}_by_day.html')
+    df_result = pm_by_daily_norm(df, 'pm_10.0', 'datetime', f'{output_file}_by_day.html')
+
+    node_name = file_name.split('_')[1].split('.')[0]
+    result = df_result.groupby(['year', 'color1']).size().reset_index(name='count')
+    result['node'] = node_name
+    red_data = result[result['color1'] == 'Red']
+    combined_df = pd.concat([combined_df, red_data], ignore_index=True)
+
+del combined_df['color1']
+combined_df['year'] = combined_df['year'].astype(str)
+combined_df['year'] = pd.Categorical(combined_df['year'], categories=sorted(combined_df['year'].unique()), ordered=True)
+combined_df = combined_df.sort_values('year')
+print(combined_df)
+
+all_years = sorted(combined_df['year'].unique())
+all_nodes = combined_df['node'].unique()
+all_combinations = pd.MultiIndex.from_product([all_years, all_nodes], names=['year', 'node']).to_frame(index=False)
+merged_df = pd.merge(all_combinations, combined_df, on=['year', 'node'], how='left')
+merged_df['count'] = merged_df['count'].fillna(0)
+print(merged_df)
+
+fig = px.bar(
+    # combined_df,
+    merged_df,
+    x='node',
+    y='count',
+    color='year',
+    barmode='group',
+    title=f'Number of exceeded values aggregated by {period}',
+    labels={'count': 'Count', 'node': 'Node'},
+    text='count'
+)
+fig.update_traces(marker=dict(line=dict(color='grey', width=2)))
+
+fig.update_layout(
+    legend_title='Year',
+    xaxis_title='Location',
+    yaxis_title='Count',
+    bargap=0.2,
+    bargroupgap=0.15,
+    template='plotly_white',
+    legend=dict(traceorder='normal'),
+    xaxis=dict(categoryorder='array', categoryarray=sorted(combined_df['year'].unique()))
+)
+output_file = os.path.join(product['plots'], 'summary.html')
+
+fig.write_html(output_file)
